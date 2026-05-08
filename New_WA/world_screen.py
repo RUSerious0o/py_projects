8d4e4117bd9493261d90744be6ea7c06c3bc7615
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2026-05-08 21:33:53 +0300

    добавлена отрисовка боевой сцены

--- New_WA/world_screen.py
+++ New_WA/world_screen.py
@@ -3,13 +3,33 @@ from pygame.sprite import Sprite
 
 
 class WorldScreen(Sprite):
+    is_battle_scene = False
+
     def __init__(self, screen: pygame.Surface, sprites: list = [], bg_color: tuple = (6, 185, 30)):
         super().__init__()
         self.screen = screen
         self.bg_color = bg_color
         self.sprites = sprites
 
+        self.current_enemy = None
+        self.player = None
+
     def update(self):
+        if not WorldScreen.is_battle_scene:
+            self.draw_world_scene()
+        else:
+            self.draw_battle_scene()
+
+
+    def add_sprite(self, sprite: Sprite, dest: tuple = (0, 0), m_scale: tuple = (200, 400)):
+        sprite.image = pygame.transform.scale(sprite.image, m_scale)
+        sprite.rect = sprite.image.get_rect()
+        sprite.rect.x = dest[0]
+        sprite.rect.y = dest[1]
+
+        self.sprites.append(sprite)
+
+    def draw_world_scene(self):
         self.screen.fill(self.bg_color)
 
         for sprite in self.sprites:
@@ -18,17 +38,24 @@ class WorldScreen(Sprite):
 
         if len(self.sprites) > 0:
             player = self.sprites[0]
+            self.player = player
             for sprite in self.sprites[1:]:
                 if player.rect.colliderect(sprite.rect):
-                    print(f'Collision {player.rect} {sprite.rect} {player.rect.colliderect(sprite.rect)}')
+                    # print(f'Collision {player.rect} {sprite.rect} {player.rect.colliderect(sprite.rect)}')
+                    self.current_enemy = sprite
+                    WorldScreen.is_battle_scene = True
 
         pygame.display.flip()
 
-    def add_sprite(self, sprite: Sprite, dest: tuple = (0, 0), m_scale: tuple = (200, 400)):
-        sprite.dest = dest
-        sprite.image = pygame.transform.scale(sprite.image, m_scale)
-        sprite.rect = sprite.image.get_rect()
-        sprite.rect.x = dest[0]
-        sprite.rect.y = dest[1]
+    def draw_battle_scene(self):
+        player = self.player
+        bg_color = (255, 125, 0)
+        player_dest = (self.screen.get_width() / 4 - player.rect.width / 2, self.screen.get_height() / 2 - player.rect.height / 2)
+        enemy_dest = (self.screen.get_width() / 4 * 3 - self.current_enemy.rect.width / 2, self.screen.get_height() / 2 - self.current_enemy.rect.height / 2)
 
-        self.sprites.append(sprite)
+        self.screen.fill(bg_color)
+
+        self.screen.blit(player.image, player_dest)
+        self.screen.blit(self.current_enemy.image, enemy_dest)
+
+        pygame.display.flip()
