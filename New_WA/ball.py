12f937a56dbd2c88fdbb17c6ac00cb383e24cea9
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2026-05-09 15:03:37 +0300

    Добавлена система урона фиолетового шара и отрисовка фиолетового шара

---
+++ New_WA/ball.py
@@ -0,0 +1,36 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+
+class Ball(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.damage = 1
+        self.image = None
+        self.speed = 30
+
+
+class PurpleBall(Ball):
+    image_path = './images/purple_ball.png'
+    damage_mult = 1
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(PurpleBall.image_path)
+        self.rect = self.image.get_rect()
+        PurpleBall.damage_mult += 1
+
+    @staticmethod
+    def reset_mult():
+        PurpleBall.damage_mult = 1
+
+
+class RedBall(Ball):
+    image_path = './images/red_ball.png'
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(RedBall.image_path)
+        self.rect = self.image.get_rect()
+        self.damage = 2 * PurpleBall.damage_mult
--- New_WA/world_screen.py
+++ New_WA/world_screen.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.sprite import Sprite
 
+from ball import *
+
 
 class WorldScreen(Sprite):
     is_battle_scene = False
@@ -13,6 +15,8 @@ class WorldScreen(Sprite):
 
         self.current_enemy = None
         self.player = None
+        self.ball = None
+        self.is_player_turn = True
         self.font_color = (255, 255, 98)
         self.font = pygame.font.Font(None, 36)
 
@@ -21,6 +25,13 @@ class WorldScreen(Sprite):
             self.draw_world_scene()
         else:
             self.draw_battle_scene()
+            if self.is_player_turn:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE and not self.ball:
+                            self.ball = PurpleBall()
+                            self.ball.rect.x = self.screen.get_width() * 0.3
+                            self.ball.rect.y = self.screen.get_height() * 0.5
 
     def add_player(self, player: Sprite):
         self.player = player
@@ -60,9 +71,22 @@ class WorldScreen(Sprite):
 
         self.screen.blit(player.image, player_dest)
         self.screen.blit(self.current_enemy.image, enemy_dest)
+        self.current_enemy.rect.x, self.current_enemy.rect.y = enemy_dest
         self.blit_battle_txt()
 
+        if self.ball:
+            self.screen.blit(self.ball.image, (self.ball.rect.x, self.ball.rect.y))
+            self.ball.rect.x += self.ball.speed
+            if self.ball.rect.colliderect(self.current_enemy.rect):
+                self.current_enemy.hp -= self.ball.damage
+                self.ball = None
+
         pygame.display.flip()
+
+        if self.current_enemy.hp <= 0:
+            WorldScreen.is_battle_scene = False
+            self.sprites.remove(self.current_enemy)
+            self.current_enemy = None
 
     def blit_battle_txt(self):
         self.screen.blit(
@@ -76,4 +100,8 @@ class WorldScreen(Sprite):
         self.screen.blit(
             self.font.render('БИТВА!', True, self.font_color),
             (self.screen.get_width() * 0.5, self.screen.get_height() * 0.3)
+        )
+        self.screen.blit(
+            self.font.render(f'Текущий множитель = {PurpleBall.damage_mult}', True, self.font_color),
+                 (self.screen.get_width() / 20, self.screen.get_height() / 20 + 50)
         )
