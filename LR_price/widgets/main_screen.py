90dc398898cbe214bd826f9a92ecc00a3f7b0541
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2026-06-15 00:52:59 +0300

    ЛР калькулятор кондеев * Рабочий, кроме функционала скрывания разделов

--- LR_price/widgets/main_screen.py
+++ LR_price/widgets/main_screen.py
@@ -4,15 +4,28 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 
 from widgets.list_item import ListItem
-
+from data import data
 
 class MainScreen(Screen):
+    __section_ids = ['preps_section', 'cleanse_section', 'refill_section', 'water_drain_section', 'other_section']
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.ids.preparation_items.add_widget(ListItem(root=self, description='LALA', price=50))
-        self.ids.preparation_items.add_widget(ListItem(root=self, description='ULALA', price=1120))
+        for section, values in data.items():
+            for line in values:
+                list_item = ListItem(root=self, description=line['name'], price=line['value'])
+                self.ids[section].ids['content'].add_widget(list_item)
+                self.ids[section].ids['content'].height += list_item.height
+                self.ids[section].height += list_item.height
 
     def change_sum(self, diff):
         self.ids.sum_label.sum_ += diff
         self.ids.sum_label.text = f'Итого: {self.ids.sum_label.sum_}'
+
+    def reset_selection(self):
+        for section in self.__section_ids:
+            for widget in self.ids[section].ids['content'].children:
+                if widget.state == 'down':
+                    widget.state = 'normal'
+                    widget.on_release()
